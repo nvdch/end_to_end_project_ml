e3255fb0c8299fbf09632bd980649edc812db478
nvdch <nealvincentchua@outlook.com>
2025-09-18 13:14:54 +0800

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 
 
 def save_object(file_path, obj):
@@ -21,17 +23,30 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models: dict):
+def evaluate_models(X_train, y_train, X_test, y_test, models: dict, params: dict):
     try:
         report = {}
 
         for i in range(len(models)):
             model = list(models.values())[i]
-            model_name = list(models.keys())[i]
+            para=params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3, n_jobs=-1, verbose=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            
             y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
             test_model_score = r2_score(y_test, y_test_pred)
-            report[model_name] = test_model_score
+
+            report[list(models.keys())[i]] = test_model_score
+
         return report
     except Exception as e:
         raise CustomException(e, sys)
