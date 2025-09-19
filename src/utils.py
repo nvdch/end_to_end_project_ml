c923ba6365e08e94cb787de60611546fe5666f1f
nvdch <nealvincentchua@outlook.com>
2025-09-19 22:32:25 +0800

    Added Flask application

--- src/utils.py
+++ src/utils.py
@@ -23,7 +23,7 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models: dict, params: dict):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
@@ -45,9 +45,16 @@ def evaluate_models(X_train, y_train, X_test, y_test, models: dict, params: dict
 
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = gs.best_score_
+            report[list(models.keys())[i]] = test_model_score
 
         return report
+    
     except Exception as e:
         raise CustomException(e, sys)
-    
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
